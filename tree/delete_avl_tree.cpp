6b46cda9295bb0037fcee9c3e4c85ac7a6c60915
snehabichkunde <bichkundesneha@gmail.com>
2023-12-23 21:05:42 +0530

    Delete from AVL tree

---
+++ tree/delete_avl_tree.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    int height;
+};
+
+int getHeight(Node* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    return node->height;
+}
+
+void updateHeight(Node* node) {
+    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+}
+
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    updateHeight(y);
+    updateHeight(x);
+
+    return x;
+}
+
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    updateHeight(x);
+    updateHeight(y);
+
+    return y;
+}
+
+int getBalance(Node* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    return getHeight(node->left) - getHeight(node->right);
+}
+
+Node* insert(Node* root, int data) {
+    if (root == nullptr) {
+        Node* newNode = new Node{data, nullptr, nullptr, 1};
+        return newNode;
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    } else {
+        return root;
+    }
+
+    updateHeight(root);
+
+    int balance = getBalance(root);
+
+    if (balance > 1 && data < root->left->data) {
+        return rightRotate(root);
+    }
+
+    if (balance < -1 && data > root->right->data) {
+        return leftRotate(root);
+    }
+
+    if (balance > 1 && data > root->left->data) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    if (balance < -1 && data < root->right->data) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+Node* minValueNode(Node* node) {
+    Node* current = node;
+    while (current->left != nullptr) {
+        current = current->left;
+    }
+    return current;
+}
+
+Node* deleteNode(Node* root, int data) {
+    if (root == nullptr) {
+        return root;
+    }
+
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    } else {
+        if (root->left == nullptr || root->right == nullptr) {
+            Node* temp = root->left ? root->left : root->right;
+
+            if (temp == nullptr) {
+                temp = root;
+                root = nullptr;
+            } else {
+                *root = *temp;
+            }
+
+            delete temp;
+        } else {
+            Node* temp = minValueNode(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+
+    if (root == nullptr) {
+        return root;
+    }
+
+    updateHeight(root);
+
+    int balance = getBalance(root);
+
+    if (balance > 1 && getBalance(root->left) >= 0) {
+        return rightRotate(root);
+    }
+
+    if (balance > 1 && getBalance(root->left) < 0) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    if (balance < -1 && getBalance(root->right) <= 0) {
+        return leftRotate(root);
+    }
+
+    if (balance < -1 && getBalance(root->right) > 0) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+void inOrderTraversal(Node* root) {
+    if (root != nullptr) {
+        inOrderTraversal(root->left);
+        cout << root->data << " ";
+        inOrderTraversal(root->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+
+    cout << "In-order traversal of the AVL tree before deletion: ";
+    inOrderTraversal(root);
+    cout << endl;
+
+    root = deleteNode(root, 30);
+
+    cout << "In-order traversal of the AVL tree after deletion: ";
+    inOrderTraversal(root);
+
+    return 0;
+}
